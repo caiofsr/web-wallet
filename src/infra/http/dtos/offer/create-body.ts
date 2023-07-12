ab4dee2f76e14dcec92e3a715c267444e4e91221
Caio Fernando <caiofsr13@gmail.com>
2023-07-12 20:15:47 -0300

    add validation on create offer endpoint

--- src/infra/http/dtos/offer/create-body.ts
+++ src/infra/http/dtos/offer/create-body.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
 export class CreateOfferBody {
+  @IsNumber()
+  @IsNotEmpty()
   quantity: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   walletCoinId: number;
 }
